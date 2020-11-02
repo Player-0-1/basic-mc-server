91c1eca9083c32beeb6841a2ec6d3c8d92ff292e
Player-0-1 <47041572+Player-0-1@users.noreply.github.com>
2020-11-02 10:26:42 -0500

    Add files via upload

---
+++ mc-server/README.md
@@ -0,0 +1,1 @@
+"# basic-mc-server" 
---
+++ mc-server/init.bat
@@ -0,0 +1,7 @@
+echo "# basic-mc-server" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Player-0-1/basic-mc-server.git
+git push -u origin main
